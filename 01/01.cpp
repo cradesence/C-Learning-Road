9b5747a51fe3a2e7a423038d275bc8965d324eca
linxunYUN <liulinxr@foxmail.com>
2022-06-10 21:03:30 +0800

    第一个C++程序

---
+++ 01/01.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: 01.cpp
+    > Author: 林讯
+    > Mail: liulinxr@foxmail.com 
+    > Created Time: Fri 10 Jun 2022 08:53:18 PM CST
+ ************************************************************************/
+
+#include<iostream>
+// 标准命名空间
+using namespace std;
+
+int main()
+{
+	// cout是标准的输出流对象
+	// endl是刷新缓冲区并换行的对象
+	cout << "hello world!" << endl;
+	return 0;
+}
